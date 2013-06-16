02f6524b861fb8dcdaa6faa4c2d311f2d3027b16
Yizhou Lin <4evermaxwell@gmail.com>
2013-06-16 01:34:47 -0400

    Create EllyPairs.cpp

---
+++ topcoder/EllyPairs.cpp
@@ -0,0 +1,28 @@
+//01zhou
+//16 Jun 2013
+
+#include <algorithm> 
+#include <vector>
+
+using namespace std;
+
+class EllysPairs
+{
+public:
+  int getDifference(vector <int> knowledge)
+	{
+		int size = knowledge.size();
+		sort(knowledge.begin(), knowledge.end());
+		int min, max;
+		min = max = knowledge[0] + knowledge[size-1];
+		for(int i=1; i<size-i-1; i++)
+		{
+			int sum = knowledge[i]+knowledge[size-i-1];
+			if(sum<min)
+				min = sum;
+			if(sum>max)
+				max = sum;
+		}
+		return max-min;
+	}
+};
