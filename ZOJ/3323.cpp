5cff76d0b9dd025ff7ad314d86249ee872a975a9
Yizhou Lin <4evermaxwell@gmail.com>
2013-06-19 21:56:05 -0300

    Create 3323.cpp

---
+++ ZOJ/3323.cpp
@@ -0,0 +1,17 @@
+//ZOJ3323
+//LANG: C++
+#include <iostream.h>
+main()
+{
+  int i,t;
+	cin>>t;
+	char psw[21];
+	while(t--)
+	{
+		cin>>psw;
+		for(i=0;psw[i]!=0;i++)
+			if(psw[i]<'0'||psw[i]>'9')
+				cout<<psw[i];
+		cout<<endl;
+	}
+}
