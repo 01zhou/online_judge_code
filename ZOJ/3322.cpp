15248839de8df8cc0c599d7a6353050ace76a302
Yizhou Lin <4evermaxwell@gmail.com>
2013-06-19 21:54:10 -0300

    Create 3322.cpp

---
+++ ZOJ/3322.cpp
@@ -0,0 +1,20 @@
+//ZOJ3322
+//LANG: C++
+#include <iostream.h>
+main()
+{
+    int t;
+	int y1,m1,d1,y2,m2,d2,ans;
+	cin>>t;
+	while(t--)
+	{
+		cin>>y1>>m1>>d1>>y2>>m2>>d2;
+		ans=(y1*1000+m1*50+d1)-(y2*1000+m2*50+d2);
+		if(ans<0)
+			cout<<"javaman\n";
+		if(ans>0)
+			cout<<"cpcs\n";
+		if(ans==0)
+			cout<<"same\n";
+	}
+}
