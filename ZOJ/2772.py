0960d1946e2a0bf43eb2d6975845f8e2d7af8c26
Yizhou Lin <4evermaxwell@gmail.com>
2013-06-24 04:54:11 -0400

    Create 2772.py

---
+++ ZOJ/2772.py
@@ -0,0 +1,21 @@
+#!python
+
+#ZOJ 2772
+#Lang: Python
+#Run Time: 20
+#Run Memory: 320
+#User Name: 01zhou
+
+import sys
+
+t = int(sys.stdin.readline())
+for i in range(t):
+  c = int(sys.stdin.readline())
+	q = int(c/25)
+	c %= 25
+	d = int(c/10)
+	c %= 10
+	n = int(c/5)
+	c %= 5
+	p = c
+	print("%d %d QUARTER(S), %d DIME(S), %d NICKEL(S), %d PENNY(S)" % (i+1, q, d, n, p))
