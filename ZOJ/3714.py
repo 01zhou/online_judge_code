267215084b5b6bb578fe5f653310ab4daefb071c
Yizhou Lin <4evermaxwell@gmail.com>
2013-06-23 23:11:36 -0400

    Create 3714.py

---
+++ ZOJ/3714.py
@@ -0,0 +1,22 @@
+#!python
+#ZOJ: 3714
+#LANG: Python
+
+import sys
+
+t = int(sys.stdin.readline())
+for i in range(t):
+  [n, m] = list(map(int, sys.stdin.readline().split()))
+	J = list(map(int, sys.stdin.readline().split()))
+	if n==m:
+		print(sum(J))
+	else:
+		now = sum(J[0:m])
+		best = now
+		for j in range(n):
+			now += J[(m+j)%n]
+			now -= J[j%n]
+			if now>best:
+				best = now
+		print(best)
+			
