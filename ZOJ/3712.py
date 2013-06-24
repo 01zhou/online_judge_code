15217714b7cdc012105c34b614330e15a53fe1da
Yizhou Lin <4evermaxwell@gmail.com>
2013-06-24 00:46:28 -0300

    Update 3712.py

--- ZOJ/3712.py
+++ ZOJ/3712.py
@@ -8,7 +8,7 @@ import sys
 
 t = int(sys.stdin.readline())
 for i in range(t):
-  [a, b, c] = list(map(int, sys.stdin.readline().split()))
+ 	[a, b, c] = list(map(int, sys.stdin.readline().split()))
 	mymax = c*c*50+(2*c+b)*b*100+(2*c+2*b+a)*a*300
 	mymin = a*a*300+(2*a+b)*b*100+(2*a+2*b+c)*c*50
 	print("%d %d" % (mymin, mymax))
